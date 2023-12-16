a3328d182abd87725f286107edcc7a6f3e6ca7eb
Abhinav Gupta <iitrabhi@gmail.com>
2023-12-16 11:57:39 -0600

    update

--- components/markers/Book.tsx
+++
@@ -1,7 +0,0 @@
-export function Book({ children }: { children: React.ReactNode }) {
-  return (
-    <em style={{ color: "#a64d79", fontWeight: "bold", fontStyle: "italic" }}>
-      {children}
-    </em>
-  );
-}
--- components/markers/Tool.tsx
+++
@@ -1,7 +0,0 @@
-export function Tool({ children }: { children: React.ReactNode }) {
-  return (
-    <em style={{ color: "#3d85c6", fontWeight: "bold", fontStyle: "normal" }}>
-      {children}
-    </em>
-  );
-}
